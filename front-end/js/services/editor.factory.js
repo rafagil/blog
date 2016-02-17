11cc0a022beef1d17619f810b91cb455377a1a15
Rafael Gil <rafaelgil@outlook.com>
2016-02-17 08:54:09 -0200

    Fixed pen editor.

--- front-end/js/services/editor.factory.js
+++ front-end/js/services/editor.factory.js
@@ -1,11 +1,14 @@
 angular.module('rafaelgil.blog.services').factory('EditorFactory', [function () {
   'use strict';
   var factory = {};
-  
+
   factory.build = function (selector) {
     return new Pen({
       editor: document.querySelector(selector),
-      list: ['bold', 'italic', 'underline']
+      list: [
+        'insertimage', 'blockquote', 'h2', 'h3', 'p', 'code', 'insertorderedlist', 'insertunorderedlist', 'inserthorizontalrule',
+        'indent', 'outdent', 'bold', 'italic', 'underline', 'createlink'
+      ]
     });
   };
 
