887712fc9c999cb993f4cf2e69ef8e3d61b4e2b7
Rafael Gil <rafaelgil@outlook.com>
2016-02-21 11:00:36 +0000

    Changed default address

--- back-end/server.js
+++ back-end/server.js
@@ -1,7 +1,7 @@
 /* global process, __dirname */
 (function() {
   'use strict';
-  var serverIp = process.env.OPENSHIFT_NODEJS_IP || 'localhost';
+  var serverIp = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
   var serverPort = process.env.OPENSHIFT_NODEJS_PORT || 3000;
   require('./app')(serverIp, serverPort, './database/blog.db');
 }());
